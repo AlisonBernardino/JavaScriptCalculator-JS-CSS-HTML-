65ac15c32c04153541f1544a905f307cddce2a0a
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-03-27 09:56:03 -0300

    Files moved

--- calculusScripts/calculatorScripts.js
+++ calculusScripts/calculatorScripts.js
@@ -1,37 +1,3 @@
-class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement) {
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currentOperandTextElement
-        this.clear()
-    }
-
-    clear() {
-        this.currentOperand = ''
-        this.previousOperand = ''
-        this.operation = undefined
-    }
-
-    delete() {
-
-    }
-
-    appendNumber(number) {
-
-    }
-
-    chooseOperation(operation) {
-
-    }
-
-    compute() {
-
-    }
-
-    updateDisplay() {
-
-    }
-}
-
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -39,11 +5,3 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
-
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
-
-numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.appendNumber()
-    })
-})
